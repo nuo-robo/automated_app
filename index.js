c61b5b5271114ef2c650058efa825eb01c5ee69e
buddha2042 <kb_collection@hotmail.com>
2024-02-17 17:22:41 -0600

    more design for static website

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const dataURL = './store/controller.json';
-fetch(dataURL)
-  .then(response => response.json())
-  .then(data => {
-    const headerContent = data.header;
-    const element = document.querySelector('.header');
-    element.innerHTML = headerContent;
-  })
-  .catch(error => console.error('Error fetching data:', error));
+function fetchDataAndUpdate() {
+  const dataURL = './store/controller.json';
+  fetch(dataURL)
+      .then(response => response.json())
+      .then(data => {
+          const headerContent = data.header;
+          const contentData = data.body;
+
+          // Select the elements to update
+          const headerElement = document.querySelector('.header');
+          const contentElement = document.querySelector('.content');
+
+          // Set inner HTML of elements to data from JSON
+          headerElement.innerHTML = headerContent;
+          contentElement.innerHTML = contentData;
+      })
+      .catch(error => console.error('Error fetching data:', error));
+}
+
+fetchDataAndUpdate();
